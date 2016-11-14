bf2d3d5bde52a3ce3675ed6c306cbdd5cb231b02
Artur Staniec <artur.staniec@creativestyle.pl>
2016-11-14 17:56:20 +0100

    fixed import

--- src/main/java/junitparams/JUnitParamsRunner.java
+++ src/main/java/junitparams/JUnitParamsRunner.java
@@ -16,7 +16,7 @@ import junitparams.internal.ParameterisedTestClassRunner;
 import junitparams.internal.ParametrizedTestMethodsFilter;
 import junitparams.internal.TestMethod;
 
-import static org.junit.internal.runners.rules.*;
+import static org.junit.internal.runners.rules.RuleFieldValidator.RULE_VALIDATOR;
 
 /**
  * <h1>JUnitParams</h1><br>
